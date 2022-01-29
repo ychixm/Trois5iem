95ef09f819d83054f0ba7a3a5f621ce41a804f0e
William Pecqueux <39126775+Milawy@users.noreply.github.com>
2022-01-29 19:14:39 +0100

    CrossingRoad prefab obstacle

--- Assets/Scripts/Map/Obstacle3D/CrossingItem3D.cs
+++ Assets/Scripts/Map/Obstacle3D/CrossingItem3D.cs
@@ -1,52 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Controller;
 using UnityEngine;
 
 public class CrossingItem3D : MonoBehaviour
 {
-
-    private Vector3 _initialPosition;
-    private Vector3 _finalposition;
-    private bool _move;
+    public Animator _anim;
 
     private void Start()
     {
-        _initialPosition = transform.position;
-        //transform.Rotate(0,180,0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (_move)
-        {
-            if (Mathf.Abs(transform.position.x - _finalposition.x) >= 0.01f)
-            {
-                transform.Translate(Vector3.left * Time.deltaTime * 2);
-            }
-            
-            else
-            {
-                
-                if (Mathf.Abs(transform.position.x - _initialPosition.x) >= 0.01f)
-                {
-                    transform.Translate(Vector3.right * Time.deltaTime * 2);
-                }
-            }
-        }
+        _anim.speed = 0;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<Runner>())
         {
-            //transform.Rotate(0,180,0);
-            _initialPosition = transform.position;
-            _finalposition = _initialPosition;
-            _finalposition.x = _initialPosition.x-4;
-            _move = true;
+            _anim.speed = 1;
         }
     }
 }
