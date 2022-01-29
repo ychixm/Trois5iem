f80b669c91bc3c4590911314b17abe3dc70bd160
Ychixm <yochix.M@hotmail.fr>
2022-01-29 09:45:16 +0100

    DirectionHelper.cs wip -> GetOppositeDirection

--- Assets/Scripts/Utility/DirectionHelper.cs
+++ Assets/Scripts/Utility/DirectionHelper.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class DirectionHelper {
 
     public static Direction GetOppositeDirection(Direction direction) {
-        return Direction.NORTH;
+        switch (direction)
+        {
+            case Direction.NORTH:
+                return Direction.SOUTH;
+            case Direction.WEST:
+                return Direction.EAST;
+            case Direction.SOUTH:
+                return Direction.NORTH;
+            case Direction.EAST:
+                return Direction.WEST;
+        }
     }
 
     public static Direction GetLeftTurnDirection(Direction direction) {
