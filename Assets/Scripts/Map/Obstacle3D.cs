43e04ef6c9aa451ca04eebace9fdd5370485e144
BenjoLaB <98610211+BenjoLaB@users.noreply.github.com>
2022-01-29 20:39:08 +0100

    button Apparition
    Pour le Hacker

--- Assets/Scripts/Map/Obstacle3D.cs
+++ Assets/Scripts/Map/Obstacle3D.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 using static ObstacleManager;
 
 public class Obstacle3D : MonoBehaviour {
-
+    public GameObject car;
+    public GameObject but;
+    public Vector3 dist , newPos;
+    public Control A;
     public virtual void Track(Control control) {
-        /* */
+        dist = new Vector3(this.transform.position.x - car.transform.position.x, 0, this.transform.position.z - car.transform.position.z);
+        newPos = new Vector3(dist.x + this.transform.position.x, car.transform.position.y+5, dist.z + this.transform.position.z);
+        Vector3 offset = newPos - this.transform.position;
+        but.transform.position = this.transform.position + Vector3.ClampMagnitude(offset,2).normalized;
+        but.transform.rotation = Quaternion.LookRotation(-Camera.main.transform.forward);
     }
 
     public virtual void Untrack(Control control) {
         /* */
+    }
+
+    public void Start()
+    {
+    }
+    public void Update()
+    {
+        Track(A);
     }
 
 }
