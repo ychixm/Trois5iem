21217580fbb81ce97f0ce852d0b812181e6f237b
Antoine Place <mail.place.a@gmail.com>
2022-01-30 15:29:55 +0100

    Helicopter

--- Assets/Scripts/Map/3D/Helicopter3D.cs
+++ Assets/Scripts/Map/3D/Helicopter3D.cs
@@ -1,70 +1,60 @@
+using Assets.Scripts.Map.Obstacles;
 using Controller;
 using UnityEngine;
 
-public class Helicopter3D : Obstacle3D
-{
+public class Helicopter3D : Actionnable3D {
+
     #region Properties
+
+    private Helicopter helicopter;
 
     public Vector3 cameraTransform = new Vector3(0,15,0);
     public float malusValue;
     public bool isSlowingRunner;
-    public bool isLightOn;
     
     private Collider _runner;
     private float _defaultMaxSpeed;
     
     #endregion
 
-    void Update()
-    {
-        GameObject car = GameObject.FindWithTag("Car");
-        float distance = Mathf.Abs(Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), car.transform.position));
-        ObstacleManager.Instance.ProcessObstacle(this, distance);
-        
-        if (isSlowingRunner)
-        {
+    void Update() {        
+        if (isSlowingRunner) {
             _runner.gameObject.GetComponent<Runner>().maxSpeed = malusValue;
         }
 
-        if (isLightOn)
-        {
-            if(transform.position.y >= cameraTransform.y)
-            {
+        if (helicopter.activated) {
+            if(transform.position.y >= cameraTransform.y) {
                 Destroy(gameObject);
             }
             transform.Translate(Vector3.up * Time.deltaTime * 2f);
-        }
-        else
-        {
-            if (transform.position.y > 5)
-            {
+        } else {
+            if (transform.position.y > 5) {
                 transform.Translate(Vector3.down * Time.deltaTime * 2f);
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.U) && transform.position.y <= 5)
-        {
-            isLightOn = true;
-        }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.GetComponent<Runner>())
-        {
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.GetComponent<Runner>()) {
             _defaultMaxSpeed = other.gameObject.GetComponent<Runner>().maxSpeed;
             isSlowingRunner = true;
             _runner = other;
         }
     }
     
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.GetComponent<Runner>())
-        {
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.GetComponent<Runner>()) {
             isSlowingRunner = false;
             _runner.gameObject.GetComponent<Runner>().maxSpeed = _defaultMaxSpeed;
             _runner = null;
         }
+    }
+
+    public override void Initialize(Obstacle obstacle) {
+        this.helicopter = (Helicopter) obstacle;
+    }
+
+    public override void OnAction() {
+        helicopter.ExecuteAction();
     }
 }
--- Assets/Scripts/Map/Data/Helicopter.cs
+++ Assets/Scripts/Map/Data/Helicopter.cs
@@ -2,12 +2,14 @@
 {
     public class Helicopter : Obstacle {
 
+        public bool activated;
+
         public Helicopter(Direction orientation) : base(orientation) {
-            
+            activated = true;
         }
 
         public override void ExecuteAction() {
-
+            activated = !activated;
         }
 
     }
