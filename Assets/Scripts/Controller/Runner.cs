973349eede2305e942a926e497f094a71a4cc2a2
William Pecqueux <39126775+Milawy@users.noreply.github.com>
2022-01-30 11:48:20 +0100

    Update test runner map

--- Assets/Scripts/Controller/Runner.cs
+++ Assets/Scripts/Controller/Runner.cs
@@ -29,33 +29,40 @@ namespace Controller
                 _rb.velocity = _rb.velocity.normalized * maxSpeed;
             }
             
-            if (Input.GetKey(KeyCode.Z))
-            {
-                _rb.AddForce(transform.forward * moveSpeed, ForceMode.Acceleration);
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                _rb.AddForce(-transform.forward * moveSpeed, ForceMode.Acceleration);
-            }
-
-            if (_rb.velocity.magnitude > 0)
-            {
-                if (Input.GetKey(KeyCode.Q))
-                {
-                    transform.Rotate(new Vector3(0f,-rotationSpeed,0f));
-                }
-                else if (Input.GetKey(KeyCode.D))
-                {
-                    transform.Rotate(new Vector3(0f,rotationSpeed,0f));
-                }
-            }
+            Accelerate();
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponentInParent<Tile3D>())
             {
-                
+                Debug.Log("trigger");
+            }
+        }
+
+        private void Accelerate()
+        {
+            if (Input.GetKey(KeyCode.Z))
+            {
+                _rb.AddForce(transform.forward * moveSpeed, ForceMode.Acceleration);
+                Turn();
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                _rb.AddForce(-transform.forward * moveSpeed, ForceMode.Acceleration);
+                Turn();
+            }
+        }
+
+        private void Turn()
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                transform.Rotate(new Vector3(0f,-rotationSpeed,0f));
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(new Vector3(0f,rotationSpeed,0f));
             }
         }
     }
